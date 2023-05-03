cb47415bb8740fe7bada7b6e671932f34ba15762
Mrlegend08 <muslimdasturchi@gmail.com>
2023-05-03 21:48:11 +0500

    header imgs add

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return <>
+    <header className="site-header">
+        <div className="container">
+            
+        </div>
+    </header>
+  </>;
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Root from './Root'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./Root";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <Root />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
